4d0069ab381cb8bb7290aee30b05fe29c247ad54
GANESH RAJENDRA MALI <156067355+ganarm@users.noreply.github.com>
2024-12-26 15:28:35 +0530

    Create op_on_matrix.java

---
+++ op_on_matrix.java
@@ -0,0 +1,6 @@
+Q. Write a menu driven program to perform the following operations on a square matrix. Write separate functions for each option.
+      i.	Check if the matrix is symmetric.
+      ii.	Display the trace of the matrix (sum of diagonal elements).
+      iii.	Check if the matrix is an upper triangular matrix.
+      iv.	Check if the matrix is a lower triangular matrix
+      v.	Check if it is an identity matrix.
