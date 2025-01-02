519bd2a751284e7fea4c6074a3b46b86e7a4645b
GANESH RAJENDRA MALI <156067355+ganarm@users.noreply.github.com>
2025-01-02 20:45:06 +0530

    Update SecondLargest.java

--- SecondLargest.java
+++ SecondLargest.java
@@ -3,7 +3,6 @@ import java.util.*;
 class SecondLargest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
         int[] arr= new int[20];
         
         System.out.print("Enter n : ");
